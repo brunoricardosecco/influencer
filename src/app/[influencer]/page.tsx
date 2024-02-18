6df3a9b50b508eccd469f374043a4d7fe66fb5aa
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2024-02-18 16:15:02 -0300

    feat: redirect on end

--- src/app/[influencer]/page.tsx
+++ src/app/[influencer]/page.tsx
@@ -4,10 +4,19 @@ import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 export default function Influencer() {
+  const onEnd = () => {
+    window.location.replace(
+      "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+    );
+  };
   return (
     <div className="min-h-screen bg-primary flex flex-col items-center py-24 relative">
       <h1 className="text-7xl font-bold mb-8">E aí, Cleiton!</h1>
-      <ReactPlayer url={"https://vimeo.com/90509568"} controls />
+      <ReactPlayer
+        url={"https://vimeo.com/90509568"}
+        controls
+        onEnded={onEnd}
+      />
       <h1 className="font-berlim text-5xl absolute bottom-24">MOK THE POKE</h1>
     </div>
   );
